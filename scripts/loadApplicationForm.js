ae5e7ea4a0bdd44059850aa53764cd13f13fd5c4
Ermal Laci <laciermal98@gmail.com>
2016-04-03 18:55:29 +0100

    Annotations

--- scripts/loadApplicationForm.js
+++ scripts/loadApplicationForm.js
@@ -30,33 +30,36 @@ $(document).ready(function () {
                     for (i = 0; i < amountofdata; i++) {    //Loop for every row selected
                       console.log(inputData[i].type)
                       if (inputData[i].type == "VARCHAR"){    //Check the type of data, depending on the type of data there will be a different display
+                        //If type is varchar then create a textfield
                         addedHTML += "<div class='mdl-grid'><div class='mdl-cell mdl-cell--8-col'><div class='mdl-textfield mdl-js-textfield applicationInputs'><input class='mdl-textfield__input input-varchar' type='text' name='input" + i + "' id='input" + i + "' maxlength='" + inputData[i].length + "'><label class='mdl-textfield__label' for=''input" + i + "'>" + inputData[i].display + "</label></div></div></div>";
+                        //The max length is limited to the length of the VARCHAR field
+                        //The display is shown in the label
                       } else if(inputData[i].type == "ENUM"){
+                        //Split the options variable at the comma to get each option in the table
                         options = inputData[i].length.split(",");
+                        //If the type is enum then create a dropdown mdl-textfield__input
                         addedHTML += "<div class='mdl-grid'><div class='mdl-cell mdl-cell--2-col'>" + inputData[i].display +"</div><div class='mdl-cell mdl-cell--4-col'><select class='input-enum' name='input" + i + "'>";
-                          for (x = 0; x < options.length; x++){
-                          addedHTML += ":<option value='" + options[x] + "'>" + options[x].toUpperCase() + "</option>";
+                          for (x = 0; x < options.length; x++){ //loops for each option
+                          addedHTML += ":<option value='" + options[x] + "'>" + options[x].toUpperCase() + "</option>"; //creates an option in the dropdown for each valid answer
                           }
                         addedHTML += "</select></div></div>";
                       } else if(inputData[i].type == "BIT"){
+                        //If the type is bit then it creates radio buttons as I use bits where 1 is true and 0 is false
                         addedHTML += "<div class='mdl-grid'><div class='mdl-cell mdl-cell--12-col'>" + inputData[i].display +"</div></div><div class='mdl-grid'><div class='mdl-cell mdl-cell--4-col'><label class='mdl-radio mdl-js-radio mdl-js-ripple-effect' for='radioButton" + i + "-option-1'><input type='radio' id='radioButton" + i + "-option-1' class='mdl-radio__button' name='input" + i + "' value='1' checked><span class='mdl-radio__label'>Yes</span></label></div><div class='mdl-cell mdl-cell--4-col'><label class='mdl-radio mdl-js-radio mdl-js-ripple-effect' for='radioButton" + i + "-option-2'><input type='radio' id='radioButton" + i + "-option-2' class='mdl-radio__button' name='options-learningneeds' value='0'><span class='mdl-radio__label'>No</span></label></div></div>";
                       } else if(inputData[i].type == "YEAR"){
-
+                        //If the type is year then create a dropdown
+                        //Split at the - as the length will hold the minimum and maximum years
                         options = inputData[i].length.split("-");
-
-                        console.log(options[0]);
-                        console.log(options[1]);
-
+                        //Make sure minYear and maxYear are integers as I use a comparison operator later
                         var minYear = parseInt(options[0]);
                         var maxYear = parseInt(options[1]);
                         var currentYear = minYear;
                         addedHTML += "<div class='mdl-grid'><div class='mdl-cell mdl-cell--6-col'>" + inputData[i].display +"</div><div class='mdl-cell mdl-cell--4-col'><select class='input-enum' name='input" + i + "'>";
-                          while (maxYear >= currentYear){
+                          while (maxYear >= currentYear){ //Loop for each of the years available
                           addedHTML += ":<option value='" + currentYear + "'>" + currentYear + "</option>";
                           currentYear += 1;
                           }
                         addedHTML += "</select></div></div>";
-
                       }
                     }
                     document.getElementById("applicantDetails").innerHTML = addedHTML;
