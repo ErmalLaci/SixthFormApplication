711a1faefb4c25f092966d864d5ecc29d3b5f5f1
Ermal Laci <laciermal98@gmail.com>
2016-04-08 13:15:25 +0100

    db ddl

--- sixthformapplication.sql
+++ sixthformapplication.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 04, 2016 at 06:45 PM
+-- Generation Time: Apr 08, 2016 at 02:01 AM
 -- Server version: 5.6.12
 -- PHP Version: 5.5.3
 
@@ -91,16 +91,22 @@ CREATE TABLE IF NOT EXISTS `applicant` (
   `tutorauthenticator` varchar(20) NOT NULL,
   PRIMARY KEY (`applicant_id`),
   UNIQUE KEY `email` (`email`),
-  KEY `applicant` (`login_id`)
+  KEY `applicant` (`login_id`),
+  KEY `selectedcourse_id` (`selectedcourses_id`),
+  KEY `gcsetaken13` (`gcsetaken13`),
+  KEY `gcsetaken12` (`gcsetaken12`),
+  KEY `gcsetaken11` (`gcsetaken11`),
+  KEY `gcsetaken10` (`gcsetaken10`),
+  KEY `gcsetaken9` (`gcsetaken9`),
+  KEY `gcsetaken8` (`gcsetaken8`),
+  KEY `gcsetaken7` (`gcsetaken7`),
+  KEY `gcsetaken6` (`gcsetaken6`),
+  KEY `gcsetaken5` (`gcsetaken5`),
+  KEY `gcsetaken4` (`gcsetaken4`),
+  KEY `gcsetaken3` (`gcsetaken3`),
+  KEY `gcsetaken2` (`gcsetaken2`),
+  KEY `gcsetaken1` (`gcsetaken1`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `applicant`
---
-
-INSERT INTO `applicant` (`login_id`, `applicant_id`, `selectedcourses_id`, `fname`, `sname`, `email`, `gcsetaken13`, `gcsetaken12`, `gcsetaken11`, `gcsetaken10`, `gcsetaken9`, `gcsetaken8`, `gcsetaken7`, `gcsetaken6`, `gcsetaken5`, `gcsetaken4`, `gcsetaken3`, `gcsetaken2`, `gcsetaken1`, `gender`, `uln`, `uci`, `postcode`, `telnumber`, `mobilenumber`, `year11school`, `year11schoolpostcode`, `year11completed`, `highdownstudent`, `parentcarerfname`, `parentcarersname`, `parentcarerpostcode`, `parentcarertelnumber`, `parentcarermobilenumber`, `contact2fname`, `contact2sname`, `contact2postcode`, `contact2telnumber`, `contact2mobilenumber`, `studentcourseinterest`, `entryrequirementsknown`, `specialrequirements`, `interviewnotes`, `subjectchoice`, `enrichment`, `tutoremail`, `studentachievements`, `learningneeds`, `learningneedsdetails`, `learningsupport`, `learningsupportdetails`, `statemented`, `statementeddetails`, `specialconsiderations`, `specialconsiderationsdetails`, `freeschoolmeals`, `fnameoftutor`, `snameoftutor`, `predictedoractualqualifications`, `tutorauthenticator`) VALUES
-(3, 3, 1, 'ermal', 'laci', 'laciermal@gmail.com', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 'male', '', '', '', '', '', '', '', 0000, b'0', '', '', '', '', '', '', '', '', '', '', b'0', b'0', '', '', 0, '', 'laciermal98@gmail.com', 'should work', b'1', 'ggewg', b'1', 'gewg', b'1', 'gerg', b'1', 'her', b'1', 'hgrh', 'hrhhh', b'1', 'hey'),
-(6, 5, 0, 'test1f', 'test1s', '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'male', '', '', '', '', '', '', '', 2014, b'1', '', '', '', '', '', '', '', '', '', '', b'0', b'0', '', '', 0, '', '', '', b'0', '', b'0', '', b'0', '', b'0', '', b'0', '', '', b'0', 'g4kh7cP8eCwBzf2toapV');
 
 -- --------------------------------------------------------
 
@@ -119,25 +125,6 @@ CREATE TABLE IF NOT EXISTS `grades` (
   KEY `subject_id` (`subject_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
---
--- Dumping data for table `grades`
---
-
-INSERT INTO `grades` (`grade_id`, `subject_id`, `predicted_grade`, `mock_result`, `actual_result`, `year_taken`) VALUES
-(1, 1, 'A*', 'A*', 'A*', 2015),
-(2, 2, 'C', 'A', 'A', 2015),
-(3, 3, 'A*', 'A', 'B', 2015),
-(4, 4, 'B', 'F', 'D', 2015),
-(5, 5, 'C', 'B', 'B', 2015),
-(6, 6, 'D', 'B', 'D', 2015),
-(7, 7, 'B', 'B', 'A', 2015),
-(8, 8, 'B', 'A*', 'A*', 2015),
-(9, 9, 'C', 'D', 'C', 2015),
-(10, 10, 'B', 'A*', 'B', 2015),
-(11, 11, 'E', 'E', 'E', 2015),
-(12, 12, 'D', 'C', 'E', 2015),
-(13, 13, 'E', 'B', 'D', 2015);
-
 -- --------------------------------------------------------
 
 --
@@ -152,16 +139,6 @@ CREATE TABLE IF NOT EXISTS `login` (
   PRIMARY KEY (`login_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`login_id`, `username`, `password`, `type`) VALUES
-(3, 'elaci', 'pass', 'applicant'),
-(4, 'admin', 'adminpass', 'admin'),
-(5, 'ttest1s', 'lZyGEWDse1KgBjwM3TJb', 'applicant'),
-(6, 'ttest1s2', 'XIuEBHR4vD81n2a6SsAr', 'applicant');
-
 -- --------------------------------------------------------
 
 --
@@ -174,13 +151,6 @@ CREATE TABLE IF NOT EXISTS `news` (
   `nameofinformation` varchar(50) NOT NULL,
   PRIMARY KEY (`news_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
--- Dumping data for table `news`
---
-
-INSERT INTO `news` (`news_id`, `information`, `nameofinformation`) VALUES
-(7, '<h3 style=''color: red''> fef </h3>', 'admin - html');
 
 -- --------------------------------------------------------
 
@@ -195,13 +165,6 @@ CREATE TABLE IF NOT EXISTS `recipient` (
   KEY `news_id` (`news_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `recipient`
---
-
-INSERT INTO `recipient` (`login_id`, `news_id`) VALUES
-(3, 7);
-
 -- --------------------------------------------------------
 
 --
@@ -210,23 +173,14 @@ INSERT INTO `recipient` (`login_id`, `news_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `selected courses` (
   `selectedcourse_id` int(11) NOT NULL AUTO_INCREMENT,
-  `applicant_id` int(11) DEFAULT NULL,
   `block_a` int(11) DEFAULT NULL,
   `block_b` int(11) DEFAULT NULL,
   `block_c` int(11) DEFAULT NULL,
   `block_d` int(11) DEFAULT NULL,
   `block_e` int(11) DEFAULT NULL,
   `courses_reasons` text NOT NULL,
-  PRIMARY KEY (`selectedcourse_id`),
-  KEY `applicant_id` (`applicant_id`)
+  PRIMARY KEY (`selectedcourse_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `selected courses`
---
-
-INSERT INTO `selected courses` (`selectedcourse_id`, `applicant_id`, `block_a`, `block_b`, `block_c`, `block_d`, `block_e`, `courses_reasons`) VALUES
-(1, 3, 1, 2, 3, 4, 5, 'I want to');
 
 -- --------------------------------------------------------
 
@@ -241,17 +195,6 @@ CREATE TABLE IF NOT EXISTS `sixth form subject` (
   `block` set('A','B','C','D','E') DEFAULT NULL,
   PRIMARY KEY (`sixthformsubject_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `sixth form subject`
---
-
-INSERT INTO `sixth form subject` (`sixthformsubject_id`, `name`, `level`, `block`) VALUES
-(1, 'Maths', 'A Level', 'A'),
-(2, 'English', 'A Level', 'B'),
-(3, 'ICT', 'A Level', 'C'),
-(4, 'Economics', 'A Level', 'D'),
-(5, 'Computing', 'A Level', 'E');
 
 -- --------------------------------------------------------
 
@@ -269,36 +212,6 @@ CREATE TABLE IF NOT EXISTS `storedinformation` (
   UNIQUE KEY `display` (`display`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
 
---
--- Dumping data for table `storedinformation`
---
-
-INSERT INTO `storedinformation` (`dataid`, `name`, `type`, `length`, `display`) VALUES
-(12, 'fname', 'VARCHAR', '25', 'First Name:'),
-(13, 'sname', 'VARCHAR', '25', 'Surname:'),
-(14, 'email', 'VARCHAR', '25', 'Email:'),
-(15, 'gender', 'ENUM', 'male,female', 'Gender:'),
-(17, 'uln', 'VARCHAR', '10', 'Unique Learner Number:'),
-(18, 'uci', 'VARCHAR', '13', 'Unique Candidate Identifier:'),
-(19, 'postcode', 'VARCHAR', '7', 'Postcode:'),
-(20, 'telnumber', 'VARCHAR', '11', 'Telephone Number:'),
-(21, 'mobilenumber', 'VARCHAR', '11', 'Mobile Number:'),
-(22, 'year11completed', 'YEAR', '2014-2016', 'Year in which you will complete (or have completed) Year 11:'),
-(23, 'year11school', 'VARCHAR', '25', 'School attended in Year 11:'),
-(24, 'year11schoolpostcode', 'VARCHAR', '7', 'Year 11 School Postcode:'),
-(25, 'highdownstudent', 'BIT', '1', 'Have you previously been a student at Highdown School?'),
-(26, 'parentcarerfname', 'VARCHAR', '25', 'First Name Of Parent/Carer:'),
-(27, 'parentcarersname', 'VARCHAR', '25', 'Surname Of Parent/Carer:'),
-(28, 'parentcarerpostcode', 'VARCHAR', '7', 'Parent/Carer Postcode:'),
-(29, 'parentcarertelnumber', 'VARCHAR', '11', 'Parent/Carer Telephone Number:'),
-(30, 'parentcarermobilenumber', 'VARCHAR', '11', 'Parent/Carer Mobile Number:'),
-(31, 'contact2fname', 'VARCHAR', '25', 'Emergency Contact 2 First Name:'),
-(32, 'contact2sname', 'VARCHAR', '25', 'Emergency Contact 2 Surname:'),
-(33, 'contact2postcode', 'VARCHAR', '7', 'Emergency Contact 2 Postcode:'),
-(34, 'contact2telnumber', 'VARCHAR', '11', 'Emergency Contact 2 Telephone Number:'),
-(35, 'contact2mobilenumber', 'VARCHAR', '11', 'Emergency Contact 2 Mobile Number:'),
-(36, 'tutoremail', 'VARCHAR', '25', 'Tutor Email:');
-
 -- --------------------------------------------------------
 
 --
@@ -311,26 +224,6 @@ CREATE TABLE IF NOT EXISTS `subject` (
   `exam_board` enum('AQA','OCR','EDEXCEL') DEFAULT NULL,
   PRIMARY KEY (`subject_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
-
---
--- Dumping data for table `subject`
---
-
-INSERT INTO `subject` (`subject_id`, `name`, `exam_board`) VALUES
-(1, 'Maths', 'AQA'),
-(2, 'English', 'AQA'),
-(3, 'History', 'AQA'),
-(4, 'Geography', 'AQA'),
-(5, 'Further Maths', 'AQA'),
-(6, 'ICT', 'AQA'),
-(7, 'Computing', 'OCR'),
-(8, 'P.E', 'AQA'),
-(9, 'Physics', 'AQA'),
-(10, 'Chemistry', 'AQA'),
-(11, 'Biology', 'AQA'),
-(12, 'Art Graphics', 'AQA'),
-(13, 'Fine Art', 'AQA'),
-(14, 'Drama', 'AQA');
 
 -- --------------------------------------------------------
 
@@ -356,7 +249,21 @@ CREATE TABLE IF NOT EXISTS `teacher` (
 -- Constraints for table `applicant`
 --
 ALTER TABLE `applicant`
-  ADD CONSTRAINT `applicant_ibfk_1` FOREIGN KEY (`login_id`) REFERENCES `login` (`login_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `applicant_ibfk_15` FOREIGN KEY (`gcsetaken1`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_1` FOREIGN KEY (`login_id`) REFERENCES `login` (`login_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `applicant_ibfk_10` FOREIGN KEY (`gcsetaken6`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_11` FOREIGN KEY (`gcsetaken5`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_12` FOREIGN KEY (`gcsetaken4`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_13` FOREIGN KEY (`gcsetaken3`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_14` FOREIGN KEY (`gcsetaken2`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_2` FOREIGN KEY (`selectedcourses_id`) REFERENCES `selected courses` (`selectedcourse_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `applicant_ibfk_3` FOREIGN KEY (`gcsetaken13`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_4` FOREIGN KEY (`gcsetaken12`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_5` FOREIGN KEY (`gcsetaken11`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_6` FOREIGN KEY (`gcsetaken10`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_7` FOREIGN KEY (`gcsetaken9`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_8` FOREIGN KEY (`gcsetaken8`) REFERENCES `grades` (`grade_id`),
+  ADD CONSTRAINT `applicant_ibfk_9` FOREIGN KEY (`gcsetaken7`) REFERENCES `grades` (`grade_id`);
 
 --
 -- Constraints for table `grades`
@@ -370,12 +277,6 @@ ALTER TABLE `grades`
 ALTER TABLE `recipient`
   ADD CONSTRAINT `recipient_ibfk_1` FOREIGN KEY (`login_id`) REFERENCES `login` (`login_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `recipient_ibfk_2` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `selected courses`
---
-ALTER TABLE `selected courses`
-  ADD CONSTRAINT `selected courses_ibfk_1` FOREIGN KEY (`applicant_id`) REFERENCES `applicant` (`applicant_id`);
 
 --
 -- Constraints for table `teacher`
