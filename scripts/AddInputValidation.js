2f2a9f9a4ec6b1cfa1fede6a67507aeda5907ef6
ErmalLaci <9620@highdown.reading.sch.uk>
2016-04-11 22:06:56 +0100

    val

--- scripts/AddInputValidation.js
+++ scripts/AddInputValidation.js
@@ -1,30 +1,30 @@
-function ValidateInput(){
-  var Type = "";
-  var Error = "";
-  var noError = true;
-  Type = document.getElementById("addInputType").value;
-  if (Type != ""){
-    console.log(Type);
-    Type = Type.toUpperCase();
-    if (!(Type == "VARCHAR" || Type == "INT" || Type == "TEXT" || Type == "DATE" || Type == "BOOLEAN" || Type == "ENUM" || Type == "BIT" || Type == "YEAR")){
-      Error += "This type doesn't exist. ";
+function ValidateInput() {
+    var Type = "";
+    var Error = "";
+    var noError = true;
+    Type = document.getElementById("addInputType").value;
+    if (Type != "") {
+        console.log(Type);
+        Type = Type.toUpperCase();
+        if (!(Type == "VARCHAR" || Type == "INT" || Type == "TEXT" || Type == "DATE" || Type == "BOOLEAN" || Type == "ENUM" || Type == "BIT" || Type == "YEAR")) {
+            Error += "This type doesn't exist. ";
+        }
+    } else {
+        Error += "You must input a type. ";
     }
-  } else{
-    Error += "You must input a type. ";
-  }
 
-  if(document.getElementById("addInputName").value == ""){
-    Error += "You must input a name. ";
-  }
+    if (document.getElementById("addInputName").value == "") {
+        Error += "You must input a name. ";
+    }
 
-  if(document.getElementById("addInputLength").value == ""){
-    //Error += "You must input a length. ";
-  }
-  console.log(Error)
-  if (Error != ""){
-    alert(Error);
-    noError = false;
-  }
-  console.log(noError);
-  return noError;
+    if (document.getElementById("addInputLength").value == "") {
+        //Error += "You must input a length. ";
+    }
+    console.log(Error)
+    if (Error != "") {
+        alert(Error);
+        noError = false;
+    }
+    console.log(noError);
+    return noError;
 }
